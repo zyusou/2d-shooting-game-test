187d93cdc6af330c656261f19f9d8bf360b27a21
zyusou <zyusou@live.jp>
2016-08-14 23:09:50 +0900

    first commit
    UnityプロジェクトをGitHubで管理することに．
    参考URL:https://keijiro.github.io/unity-github-tutor/

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    // 移動速度
+    public float speed = 5;
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	    while (true)
+	    {
+            //弾をプレイヤと同じ位置/角度で作成
+	        Instantiate(bullet, transform.position, transform.rotation);
+
+            yield return new WaitForSeconds(0.05f);
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    float x = Input.GetAxisRaw("Horizontal");
+	    float y = Input.GetAxisRaw("Vertical");
+
+        //　移動方向を計算
+	    Vector2 direction = new Vector2(x, y).normalized;
+
+        //　アタッチされたコンポーネントに反映
+	    GetComponent<Rigidbody2D>().velocity = direction*speed;
+
+	}
+}
